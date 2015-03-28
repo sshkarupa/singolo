9dbcdd2a27599cb8bb480b54ecf081f4444b9d90
sshkarupa <s.shkarupa@gmail.com>
2015-03-28 12:50:33 +0100

    init

---
+++ js/common.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+	$('.menu-button').click(function(){
+		$('.main-menu ul').slideToggle();
+	});
+
+
+	//Цели для Яндекс.Метрики и Google Analytics
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("#form").submit(function(e) {
+		e.preventDefault;
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+			}, 1000);
+		});
+	});
+
+});
